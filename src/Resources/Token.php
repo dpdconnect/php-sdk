d0d377b4895a95e70752eb7c571576c2054a443e
DPD Development <pluginsupport@dpd.nl>
2021-11-18 11:32:29 +0100

    1.1.6

--- src/Resources/Token.php
+++ src/Resources/Token.php
@@ -35,6 +35,11 @@ class Token
      */
     public function getPublicJWTToken($username, $password)
     {
+        $result = $this->getCachedPublicJWTToken($username);
+        if ($result && $this->isTokenValid($result)) {
+            return $result;
+        }
+
         $requestBody = [
             'username' => $username,
             'password' => $password,
@@ -60,6 +65,65 @@ class Token
             return $response;
         }
 
+        $this->storeCachedPublicJWTToken($decoded['token'], $username);
+
         return $decoded['token'];
+    }
+
+    /**
+     * @param $token
+     *
+     * @return bool
+     */
+    private function isTokenValid($token)
+    {
+        $explodedToken = explode('.', $token);
+
+        // Check if token has header, payload and signature
+        if (count($explodedToken) != 3) {
+            return false;
+        }
+
+        list($header, $payload, $signature) = $explodedToken;
+
+        $payload = json_decode(base64_decode($payload), true);
+
+        // Check if token is expired
+        // Subtract 5 minutes of token to prevent returning a shortly-expiring token
+        if (time() >= (int)$payload['exp'] - (5*60)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $username
+     *
+     * @return false|mixed
+     */
+    private function getCachedPublicJWTToken($username)
+    {
+        $filename = sys_get_temp_dir() . '/dpd/' . sha1('dpd-products' . date('YmdH') . serialize($username));
+
+        if (!file_exists($filename) || filesize($filename) == 0) {
+            return false;
+        }
+
+        return unserialize(file_get_contents($filename));
+    }
+
+    /**
+     * @param $token
+     * @param $username
+     */
+    private function storeCachedPublicJWTToken($token, $username)
+    {
+        if (!file_exists(sys_get_temp_dir() . '/dpd/')) {
+            mkdir(sys_get_temp_dir() . '/dpd/');
+        }
+
+        $filename = sys_get_temp_dir() .'/dpd/' . sha1('dpd-products' . date('YmdH') . serialize($username));
+        file_put_contents($filename, serialize($token));
     }
 }
